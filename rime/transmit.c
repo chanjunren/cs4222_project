a7eda53ccd4c952ac5dc2bd2b5ca14b38404a868
mononokehime14 <952284692@qq.com>
2022-02-22 15:56:34 +0800

    A3 transmit code

--- rime/transmit.c
+++ rime/transmit.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "net/rime/rime.h"
 #include <stdio.h>
+#include <math.h>
 /*---------------------------------------------------------------------------*/
 
 
@@ -15,16 +16,19 @@ AUTOSTART_PROCESSES(&transmit_process);
 
 static const struct unicast_callbacks unicast_callbacks = {};
 static struct unicast_conn uc;
+static int length = 0;
+static char message[50];
+static int i;
 
 static void send(char message[], int size){
 	linkaddr_t addr;
-	printf("%s\n",message);
+	//printf("%s\n",message);
    packetbuf_copyfrom(message, strlen(message));
 
    // COMPUTE THE ADDRESS OF THE RECEIVER FROM ITS NODE ID, FOR EXAMPLE NODEID 0xBA04 MAPS TO 0xBA AND 0x04 RESPECTIVELY
    // In decimal, if node ID is 47620, this maps to 186 (higher byte) AND 4 (lower byte)
-   addr.u8[0] = 0x1f; // HIGH BYTE or 186 in decimal
-   addr.u8[1] = 0x0d; // LOW BYTE or 4 in decimal
+   addr.u8[0] = 46; // HIGH BYTE or 186 in decimal
+   addr.u8[1] = 2; // LOW BYTE or 4 in decimal
    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
          unicast_send(&uc, &addr);
    }
@@ -36,7 +40,17 @@ PROCESS_THREAD(transmit_process, ev, data) {
 	PROCESS_EXITHANDLER(unicast_close(&uc);)
 	PROCESS_BEGIN();
 	unicast_open(&uc, 146, &unicast_callbacks);
+   static struct etimer et;
 
+   for(i = 0;i < 160;i++){
+      /* Delay 0.25 seconds */
+      etimer_set(&et, CLOCK_SECOND * 0.0625);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      
+      sprintf(message, "%d",i+1);
+      length = (int)((ceil(log10(i+1))+1));
+      send(message, length);
+   }
 
 
    PROCESS_END();
